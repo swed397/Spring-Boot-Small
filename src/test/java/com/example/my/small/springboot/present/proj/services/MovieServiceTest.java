22b45b0dc47afdd79e28005cd6585b92083fb2fc
Rodion <swed197@mail.ru>
2022-12-21 01:31:42 +0500

    Fast fix

--- src/test/java/com/example/my/small/springboot/present/proj/services/MovieServiceTest.java
+++ src/test/java/com/example/my/small/springboot/present/proj/services/MovieServiceTest.java
@@ -1,13 +1,9 @@
 package com.example.my.small.springboot.present.proj.services;
 
-import com.example.my.small.springboot.present.proj.entities.Movie;
-import com.example.my.small.springboot.present.proj.repositories.MovieRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
